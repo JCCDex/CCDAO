4268b7f2a84944a30e111fb37f3b77f0df4faff4
onewirter <1316788824@qq.com>
2022-05-12 17:55:56 +0800

    ==

--- web/ccdao-web/src/components/Foot.vue
+++ web/ccdao-web/src/components/Foot.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="foot">
     <p class="foottext">Copyright © 2022 by The CCDAO. All Rights Reserved.</p>
-
-    <img src="../assets/foottag.svg" style="position: absolute; right: 50px; top: 20px" />
+    <img src="../assets/foottag.svg" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Foot.vue",
+};
 </script>
 
 <style>
@@ -15,11 +16,20 @@ export default {};
   width: 100%;
   height: 60px;
   background: rgba(246, 249, 253, 1);
+  position: relative;
+  text-align: center;
 }
 .foottext {
-  width: 1440px;
-  height: 60px;
+  margin-top: 25px;
+  width: 100%;
   color: rgba(112, 112, 112, 1);
   font-size: 12px;
+}
+.foot img {
+  position: absolute;
+  width: 74px;
+  height: 20.5px;
+  right: 50px;
+  top: 15px;
 }
 </style>
--- web/ccdao-web/src/components/Holds.vue
+++ web/ccdao-web/src/components/Holds.vue
@@ -1,67 +1,83 @@
 <template>
-    <div class="boxtwo">
-        <a name=Holds></a>
-        <!-- <img src="../assets/bgimg2.svg" style="width: 100%;position: absolute; z-index: -1;"> -->
-        <div>
-            <p style="margin-left: 42.6%;margin-top: 8.8%;font-size: 50px;line-height: 50px;">$CCDAO</p>
-            <p style="margin-left: 43.6%;margin-top: 16.6%;font-size: 40px;line-height: 50px;">{{$t("HOLDING")}}</p>
-        </div>
-        <div class="holdout">
-            <div class="hold">
-            <div class="holdbox">
-                <img src="../assets/tag.svg" style="position: absolute;">
-                <p style="margin-left: 160px;margin-top: 20px;font-size: 14px;line-height: 40px;">{{$t("ETH chain Positions")}}</p>
-                <p style="margin-left: 160px;margin-top: 60px;font-size: 30px;">1000000CCDAO</p>
-            </div>
-            <div class="holdbox" style="margin-top:50px;">
-                <img src="../assets/tag.svg" style="position: absolute;">
-                <p style="margin-left: 160px;margin-top: 20px;font-size: 14px;line-height: 40px;"> {{$t("SWTC chain Positions")}}</p>
-                <p style="margin-left: 160px;margin-top: 60px;font-size: 30px;">1000000CCDAO</p>
-            </div>
-            </div>
-        <img src="../assets/bgimg2.svg" style="width: 100%;position: relative; z-index: -1;"> 
-        </div>
+  <div class="boxtwo">
+    <a name="Holds"></a>
+    <!-- <img src="../assets/bgimg2.svg" style="width: 100%;position: absolute; z-index: -1;"> -->
+    <div class="boxtwotext">
+      <p class="p1" style="font-size: 50px; line-height: 50px">$CCDAO</p>
+      <p class="p1" style="font-size: 40px; line-height: 50px">{{ $t("HOLDING") }}</p>
     </div>
+    <div class="holdout">
+      <div class="hold">
+        <div class="holdbox">
+          <img src="../assets/tag.svg" style="position: absolute" />
+          <p class="p2" style="margin-left: 160px; margin-top: 20px; font-size: 14px; line-height: 40px">
+            {{ $t("ETH chain Positions") }}
+          </p>
+          <p class="p2" style="margin-left: 160px; margin-top: 60px; font-size: 30px">1000000CCDAO</p>
+        </div>
+        <div class="holdbox" style="margin-top: 50px">
+          <img src="../assets/tag.svg" style="position: absolute" />
+          <p class="p2" style="margin-left: 160px; margin-top: 20px; font-size: 14px; line-height: 40px">
+            {{ $t("SWTC chain Positions") }}
+          </p>
+          <p class="p2" style="margin-left: 160px; margin-top: 60px; font-size: 30px">1000000CCDAO</p>
+        </div>
+      </div>
+      <img src="../assets/bgimg2.svg" style="width: 100%; position: relative; z-index: -1" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"Holds"
-}
+  name: "Holds",
+};
 </script>
 
 <style>
-    .boxtwo{
-        width: 100%;
-        position: relative;
-        /* background-image: url("../assets/bgimg2.svg"); */
-    }
-    p{
-        width: 183px;
-        height: 50px;
-        mix-blend-mode: normal;
-        font-family: PingFangSC-Semibold, sans-serif;
-        position: absolute;
-        color: rgba(255, 255, 255, 1);
-    }
-    .holdout{
-        display: flex;
-        justify-content: center;
-    }
-    .hold{
-        /* margin-left: 29.1%; */
-        position: absolute;
-        margin-top: 30%;
-        width: 600px;
-        height: 290px;
-    }
-    .holdbox{
-        width: 600px;
-        height: 120px;
-        background: rgba(96.00000000000001, 194.00000000000006, 248, 1);
-        background-blend-mode: normal;
-        border-radius: 110px;
-        mix-blend-mode: normal;
-    }
-
+.boxtwo {
+  width: 100%;
+  position: relative;
+  /* background-image: url("../assets/bgimg2.svg"); */
+}
+.boxtwotext {
+  position: absolute;
+  width: 100%;
+  top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.p1 {
+  mix-blend-mode: normal;
+  font-family: PingFangSC-Semibold, sans-serif;
+  color: rgba(255, 255, 255, 1);
+}
+.p2 {
+  width: 183px;
+  height: 50px;
+  mix-blend-mode: normal;
+  font-family: PingFangSC-Semibold, sans-serif;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+}
+.holdout {
+  display: flex;
+  justify-content: center;
+}
+.hold {
+  /* margin-left: 29.1%; */
+  position: absolute;
+  margin-top: 30%;
+  width: 600px;
+  height: 290px;
+}
+.holdbox {
+  width: 600px;
+  height: 120px;
+  background: rgba(96.00000000000001, 194.00000000000006, 248, 1);
+  background-blend-mode: normal;
+  border-radius: 110px;
+  mix-blend-mode: normal;
+}
 </style>
