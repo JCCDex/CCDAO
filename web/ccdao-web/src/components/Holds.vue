16d6c94f23b952f2fa07a2047b144301d943b604
onewirter <1316788824@qq.com>
2022-06-02 15:35:17 +0800

    phone CCDAO 初

--- web/ccdao-web/src/components/Holds.vue
+++ web/ccdao-web/src/components/Holds.vue
@@ -2,27 +2,25 @@
   <div class="hold-container" id="holding">
     <!-- <img src="../assets/bgimg2.svg" style="width: 100%;position: absolute; z-index: -1;"> -->
     <div class="boxtwotext">
-      <span class="p1" style="font-size: 50px; line-height: 50px">{{ $t("message.$CCDAO") }}</span>
-      <span class="p1" style="font-size: 40px; line-height: 50px">{{ $t("message.HOLDING") }}</span>
-      <div class="holdout">
-        <div class="hold">
-          <div class="holdbox">
-            <img src="../assets/tag.svg" style="position: absolute" />
-            <p class="p2" style="margin-left: 160px; margin-top: 20px; font-size: 14px; line-height: 40px">
-              {{ $t("message.ETH_chain_Positions") }}
-            </p>
-            <p class="p2" style="margin-left: 160px; margin-top: 60px; font-size: 30px">
-              {{ ethCcdao }}&nbsp;{{ $t("message.CCDAO") }}
-            </p>
-          </div>
-          <div class="holdbox" style="margin-top: 50px">
-            <img src="../assets/tag.svg" style="position: absolute" />
-            <p class="p2" style="margin-left: 160px; margin-top: 20px; font-size: 14px; line-height: 40px">
-              {{ $t("message.SWTC_chain_Positions") }}
-            </p>
-            <p class="p2" style="margin-left: 160px; margin-top: 60px; font-size: 30px">
-              {{ swtcCcdao }}&nbsp;{{ $t("message.CCDAO") }}
-            </p>
+      <div class="holdscss">
+        <span class="p1 p11">{{ $t("message.$CCDAO") }}</span>
+        <span class="p1 p12">{{ $t("message.HOLDING") }}</span>
+        <div class="holdout">
+          <div class="hold">
+            <div class="holdbox">
+              <img class="holdboximg" src="../assets/tag.svg" style="position: absolute" />
+              <p class="p2 p21">
+                {{ $t("message.ETH_chain_Positions") }}
+              </p>
+              <p class="p2 p22">{{ ethCcdao }}&nbsp;{{ $t("message.CCDAO") }}</p>
+            </div>
+            <div class="holdbox holdboxplace">
+              <img class="holdboximg" src="../assets/tag.svg" style="position: absolute" />
+              <p class="p2 p21">
+                {{ $t("message.SWTC_chain_Positions") }}
+              </p>
+              <p class="p2 p22">{{ swtcCcdao }}&nbsp;{{ $t("message.CCDAO") }}</p>
+            </div>
           </div>
         </div>
       </div>
--- web/ccdao-web/src/components/Member.vue
+++ web/ccdao-web/src/components/Member.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="member-container" id="membership">
     <div class="member">
-      <p class="membership">{{ $t("message.Membership") }}</p>
-      <div class="memberbox">
-        <div>
-          <p class="mca">{{ $t("message.My_CCDAO_Amount") }}</p>
-          <p class="mcanum">{{ ccdaoNum.toLocaleString() }}&nbsp;{{ $t("message.CCDAO") }}</p>
-        </div>
-        <div v-show="ccdaoNum >= 10000" style="display: flex; align-items: center; font-size: 14px">
-          <span style="color: rgba(161, 166, 169, 1)">{{ $t("message.status") }}</span>
-          <span style="color: rgba(51, 147, 230, 1); margin-right: 10px">{{ $t("message.YES") }}</span>
-          <img src="../assets/member.svg" alt="" />
-        </div>
-        <div v-show="ccdaoNum < 10000" style="display: flex; align-items: center; font-size: 14px">
-          <span style="color: rgba(161, 166, 169, 1)">{{ $t("message.status") }}</span>
-          <span style="color: rgba(51, 147, 230, 1); margin-right: 10px">{{ $t("message.NO") }}</span>
-          <img src="../assets/member1.svg" alt="" />
-        </div>
-
-        <div class="memberboxtext">
-          <div style="display: flex; justify-content: space-between">
-            <p style="margin: 0px; font-size: 20px">{{ $t("message.Membership_Requirements") }}</p>
-            <img src="../assets/buyarrow.svg" class="memberarrow" />
+      <div class="memberdiv">
+        <p class="membership">{{ $t("message.Membership") }}</p>
+        <div class="memberbox">
+          <div>
+            <p class="mca">{{ $t("message.My_CCDAO_Amount") }}</p>
+            <p class="mcanum">{{ ccdaoNum.toLocaleString() }}&nbsp;{{ $t("message.CCDAO") }}</p>
           </div>
-          <span class="needCcdao">
-            {{ $t("message.$CCDAO_Member_need_to_hold_10000CCDAO_at_least") }}
-          </span>
+          <div v-show="ccdaoNum >= 10000" style="display: flex; align-items: center; font-size: 14px">
+            <span style="color: rgba(161, 166, 169, 1)">{{ $t("message.status") }}</span>
+            <span style="color: rgba(51, 147, 230, 1); margin-right: 10px">{{ $t("message.YES") }}</span>
+            <img class="memberimg" src="../assets/member.svg" alt="" />
+          </div>
+          <div v-show="ccdaoNum < 10000" style="display: flex; align-items: center; font-size: 14px">
+            <span style="color: rgba(161, 166, 169, 1)">{{ $t("message.status") }}</span>
+            <span style="color: rgba(51, 147, 230, 1); margin-right: 10px">{{ $t("message.NO") }}</span>
+            <img class="memberimg" src="../assets/member1.svg" alt="" />
+          </div>
+
+          <div class="memberboxtext">
+            <div style="display: flex; justify-content: space-between">
+              <p class="mr">{{ $t("message.Membership_Requirements") }}</p>
+              <img src="../assets/buyarrow.svg" class="memberarrow" />
+            </div>
+            <span class="needCcdao">
+              {{ $t("message.$CCDAO_Member_need_to_hold_10000CCDAO_at_least") }}
+            </span>
+          </div>
         </div>
       </div>
     </div>
