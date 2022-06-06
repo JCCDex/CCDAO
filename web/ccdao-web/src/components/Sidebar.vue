8aa593387fb1a3e73e3c2e8cb3b9be1e2e84f37b
onewirter <1316788824@qq.com>
2022-06-06 15:09:49 +0800

    sidebar

--- web/ccdao-web/src/components/Sidebar.vue
+++ web/ccdao-web/src/components/Sidebar.vue
@@ -1,41 +1,43 @@
 <template>
-  <div id="arr" class="sidebardiv" v-show="sidebarBool" :style="{ height: sidebarHeight + 'px' }">
-    <div class="between">
-      <span></span>
-      <img class="fork" src="../assets/fork.svg" alt="" @click="downSidebar()" />
-    </div>
-    <div class="sidebarbt between" @click="isShowCommunity = !isShowCommunity">
-      <span>{{ $t("message.Community") }}</span>
-      <img v-show="!isShowCommunity" class="sidebarupimg" src="../assets/sidebardownimg.svg" alt="" />
-      <img v-show="isShowCommunity" class="sidebarupimg" src="../assets/sidebarupimg.svg" alt="" />
-    </div>
-    <div v-show="isShowCommunity" class="sidebarselects">
-      <a href="" target="_blank" class="sidebarselect" @click="downSidebar()">{{ $t("message.Twitter") }}</a>
-      <a href="" target="_blank" class="sidebarselect" @click="downSidebar()">{{ $t("message.Discord") }}</a>
-      <a href="" target="_blank" class="sidebarselect" @click="downSidebar()">{{ $t("message.Telegram") }}</a>
-    </div>
-    <div class="sidebarbt between" @click="isShowCcdao = !isShowCcdao">
-      <span>{{ $t("message.$CCDAO") }}</span>
-      <img v-show="!isShowCcdao" class="sidebarupimg" src="../assets/sidebardownimg.svg" alt="" />
-      <img v-show="isShowCcdao" class="sidebarupimg" src="../assets/sidebarupimg.svg" alt="" />
-    </div>
-    <div v-show="isShowCcdao" class="sidebarselects">
-      <a href="#holding" class="sidebarselect" @click="downSidebar()">{{ $t("message.Holding") }}</a>
-      <a href="#trade" class="sidebarselect" @click="downSidebar()">{{ $t("message.How_to_Buy") }}</a>
-      <a href="#contract" class="sidebarselect" @click="downSidebar()">{{ $t("message.Contract_Address") }}</a>
-    </div>
-    <a href="#membership">
-      <div class="sidebarbt between" @click="downSidebar()">
-        <span>{{ $t("message.Membership") }}</span>
+  <div class="sidebarbox" @click.stop="downSidebar()">
+    <div @click.stop="" id="arr" class="sidebardiv" v-show="sidebarBool" :style="{ height: sidebarHeight + 'px' }">
+      <div class="between">
         <span></span>
+        <img class="fork" src="../assets/fork.svg" alt="" @click="downSidebar()" />
       </div>
-    </a>
-    <a href="#multi">
-      <div class="sidebarbt between" @click="downSidebar()">
-        <span>{{ $t("message.Multi-Signers") }}</span>
-        <span></span>
+      <div class="sidebarbt between" @click="isShowCommunity = !isShowCommunity">
+        <span>{{ $t("message.Community") }}</span>
+        <img v-show="!isShowCommunity" class="sidebarupimg" src="../assets/sidebardownimg.svg" alt="" />
+        <img v-show="isShowCommunity" class="sidebarupimg" src="../assets/sidebarupimg.svg" alt="" />
       </div>
-    </a>
+      <div v-show="isShowCommunity" class="sidebarselects">
+        <a href="" target="_blank" class="sidebarselect" @click="downSidebar()">{{ $t("message.Twitter") }}</a>
+        <a href="" target="_blank" class="sidebarselect" @click="downSidebar()">{{ $t("message.Discord") }}</a>
+        <a href="" target="_blank" class="sidebarselect" @click="downSidebar()">{{ $t("message.Telegram") }}</a>
+      </div>
+      <div class="sidebarbt between" @click="isShowCcdao = !isShowCcdao">
+        <span>{{ $t("message.$CCDAO") }}</span>
+        <img v-show="!isShowCcdao" class="sidebarupimg" src="../assets/sidebardownimg.svg" alt="" />
+        <img v-show="isShowCcdao" class="sidebarupimg" src="../assets/sidebarupimg.svg" alt="" />
+      </div>
+      <div v-show="isShowCcdao" class="sidebarselects">
+        <a href="#holding" class="sidebarselect" @click="downSidebar()">{{ $t("message.Holding") }}</a>
+        <a href="#trade" class="sidebarselect" @click="downSidebar()">{{ $t("message.How_to_Buy") }}</a>
+        <a href="#contract" class="sidebarselect" @click="downSidebar()">{{ $t("message.Contract_Address") }}</a>
+      </div>
+      <a href="#membership">
+        <div class="sidebarbt between" @click="downSidebar()">
+          <span>{{ $t("message.Membership") }}</span>
+          <span></span>
+        </div>
+      </a>
+      <a href="#multi">
+        <div class="sidebarbt between" @click="downSidebar()">
+          <span>{{ $t("message.Multi-Signers") }}</span>
+          <span></span>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -57,14 +59,12 @@ export default {
   methods: {
     downSidebar() {
       this.$store.commit("downSidebar");
+      this.isShowCommunity = false;
+      this.isShowCcdao = false;
     },
   },
   mounted() {
     this.sidebarHeight = window.screen.height;
-    // document.addEventListener('click',()=>{
-    //   console.log(this.$store.state.sidebarBool);
-    // this.$store.commit("downSidebar");
-    // });
   },
 };
 </script>
@@ -81,12 +81,16 @@ a {
   justify-content: space-between;
   align-items: center;
 }
+.sidebarbox {
+  width: 100%;
+  position: fixed;
+  z-index: 10000;
+  left: 0px;
+}
 .sidebardiv {
   // display: none;
   width: 81.25%;
   background: rgba(0, 21, 36, 0.8);
-  position: fixed;
-  z-index: 100000;
   left: 0px;
   color: white;
   .fork {
