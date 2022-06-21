e65a6ee291cd0f70cc08c11844b8b30c1c86cd03
onewirter <1316788824@qq.com>
2022-06-21 13:36:09 +0800

    node-cron

--- web/ccdao-web/setCcdao.js
+++ web/ccdao-web/setCcdao.js
@@ -3,20 +3,35 @@ const { default: BigNumber } = require("bignumber.js");
 const fs = require("fs");
 const path = require("path");
 const file = path.join(__dirname, "./public/config.json");
+const CronJob = require("cron").CronJob;
 
 var ccdao = {};
-axios
-  .get("https://ijib059e8792d5.jccdex.cn/info/alltickers")
-  .then((res) => {
-    ccdao = res.data.data["JJCC-JUSDT"];
-    const data = JSON.parse(fs.readFileSync(file, "utf8"));
-    let x = new BigNumber(ccdao[6]);
-    let y = new BigNumber(ccdao[1]);
-    data.totalVolumeTraded = x.integerValue(1).toNumber();
-    data.fullyDilutedValuation = x.times(y).integerValue(1).toNumber();
-    fs.writeFileSync(file, JSON.stringify(data), "utf8");
-    console.log("success");
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+const setCcdao = () => {
+  axios
+    .get("https://ijib059e8792d5.jccdex.cn/info/alltickers")
+    .then((res) => {
+      //  ccdao = res.data.data["JJCC-JUSDT"];
+      ccdao = res.data.data["CCDAO-JUSDT"];
+      const data = JSON.parse(fs.readFileSync(file, "utf8"));
+      let x = new BigNumber(ccdao[6]);
+      let y = new BigNumber(ccdao[1]);
+      data.totalVolumeTraded = x.integerValue(1).toNumber();
+      data.fullyDilutedValuation = x.times(y).integerValue(1).toNumber();
+      fs.writeFileSync(file, JSON.stringify(data), "utf8");
+      console.log("success");
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+var job = new CronJob(
+  "*/10 * * * * *",
+  function () {
+    setCcdao();
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
+job.start();
