7e8e22258e923470b34c610509ebd5b27563557e
GinMu <gin.musheng@gmail.com>
2022-06-25 16:10:44 +0800

    fixed cron period

--- web/ccdao-web/setCcdao.js
+++ web/ccdao-web/setCcdao.js
@@ -9,20 +9,24 @@ const setCcdao = () => {
   axios
     .get("https://ijib059e8792d5.jccdex.cn/info/alltickers")
     .then((res) => {
-      // const ccdao = res.data.data["JJCC-JUSDT"];
       const ccdao = res.data.data["CCDAO-JUSDT"];
       const data = JSON.parse(fs.readFileSync(file, "utf8"));
       let x = new BigNumber(ccdao[6]);
       let y = new BigNumber(ccdao[1]);
+      if (x.isNaN() || y.isNaN()) {
+        return;
+      }
       data.totalVolumeTraded = x.integerValue(1).toNumber();
       data.fullyDilutedValuation = x.times(y).integerValue(1).toNumber();
       fs.writeFileSync(file, JSON.stringify(data, null, 2), "utf8");
     })
-    .catch(function (error) {});
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
 var job = new CronJob(
-  "* */10 * * * *",
+  "1 */10 * * * *",
   function () {
     setCcdao();
   },
