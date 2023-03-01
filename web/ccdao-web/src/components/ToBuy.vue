f9128ce6556d116e01882c4d009162ed39a68489
onewirter <1316788824@qq.com>
2023-03-01 15:56:12 +0800

    CCDAO adds four links and related UI

--- web/ccdao-web/src/components/ToBuy.vue
+++ web/ccdao-web/src/components/ToBuy.vue
@@ -10,18 +10,42 @@
         <div class="buyadd">
           <span class="buy">{{ $t("message.You_can_buy_$CCDAO_in") }}</span>
           <img src="../assets/buyarrow.svg" class="buyimg" />
-          <a class="buyaddimport" href="https://letsdex.com" target="_blank">
-            <div class="buyaddbox buylocation1">
-              <img src="../assets/buyaddtag1.svg" class="buytag" />
-              <p class="buyaddtext">{{ $t("message.LetsDEX") }}</p>
+          <div>
+            <div>
+              <a href="https://letsdex.com" target="_blank">
+                <div class="buyaddbox buylocation1">
+                  <img src="../assets/buyaddtag1.svg" class="buytag" />
+                  <p class="buyaddtext">{{ $t("message.LetsDEX") }}</p>
+                </div>
+              </a>
+              <a href="https://app.uniswap.org/" target="_blank">
+                <div class="buyaddbox buylocation2">
+                  <img src="../assets/buyaddtag2.svg" class="buytag" />
+                  <p class="buyaddtext">{{ $t("message.Uniswap") }}</p>
+                </div>
+              </a>
             </div>
-          </a>
-          <a class="buyaddimport" href="https://app.uniswap.org/" target="_blank">
-            <div class="buyaddbox buylocation2">
-              <img src="../assets/buyaddtag2.svg" class="buytag" />
-              <p class="buyaddtext">{{ $t("message.Uniswap") }}</p>
+            <div>
+              <a
+                href="https://pancakeswap.finance/swap?chain=bsc&outputCurrency=0x200F73AbA0D6F8919086F64035eF900669ea2De2"
+                target="_blank"
+              >
+                <div class="buyaddbox buylocation3">
+                  <img src="../assets/buyaddtag3.svg" class="buytag" />
+                  <p class="buyaddtext">{{ $t("message.PancakeSwap") }}</p>
+                </div>
+              </a>
+              <a
+                href="https://quickswap.exchange/#/swap?swapIndex=1&currency0=ETH&currency1=0xee546f831533a913848b72f36a9D5E437F63dbB9"
+                target="_blank"
+              >
+                <div class="buyaddbox buylocation4">
+                  <img src="../assets/buyaddtag4.svg" class="buytag" />
+                  <p class="buyaddtext">{{ $t("message.Quickswap") }}</p>
+                </div>
+              </a>
             </div>
-          </a>
+          </div>
         </div>
       </div>
     </div>
