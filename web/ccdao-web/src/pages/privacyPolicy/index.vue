a0d06d9233e9668ef85defc21833ce714fa77c56
onewirter <1316788824@qq.com>
2023-08-21 13:27:52 +0800

    Adjust the official website terms and route adjustment

---
+++ web/ccdao-web/src/pages/privacyPolicy/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="roll">
+    <Netalter></Netalter>
+    <component :is="is"></component>
+  </div>
+</template>
+
+<script>
+import en from "./en.vue";
+import zh from "./zh.vue";
+import Netalter from "../../components/Netalter";
+
+export default {
+  data() {
+    return {
+      is: "",
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    const { lang } = to.params;
+    next((vm) => {
+      if (lang) {
+        vm.is = lang;
+      } else {
+        vm.is = vm.$i18n.locale;
+      }
+    });
+  },
+  components: {
+    en,
+    zh,
+    Netalter,
+  },
+};
+</script>
