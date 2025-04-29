2c0157c9d792cb3205d34b5c9062439ca6096006
chtian <chtian@anfengde.com>
2025-04-29 21:40:26 +0800

    Migrate CCDAO ERC20 done

--- contract/foundry/CCDAO_ERC20/src/interface/IERC223Recipient.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
-
-/**
-IERC223 Recipient call back hook definition
-*/
-interface IERC223Recipient {
-  function tokenReceived(address from, uint256 value, bytes calldata data) external;
-}
