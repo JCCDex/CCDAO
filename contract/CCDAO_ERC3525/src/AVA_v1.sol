c0cde4e2f51d10c6e1a1d5de419b3fcd2d794d83
chtian <chtian@anfengde.com>
2025-06-16 12:12:39 +0800

    format ava v1

--- contract/CCDAO_ERC3525/src/AVA_v1.sol
+++ contract/CCDAO_ERC3525/src/AVA_v1.sol
@@ -8,7 +8,7 @@ import "./ERC3525SlotEnumerable.sol";
 contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
     // Base URI for computing {tokenURI} and {slotURI}
     string private _avaURI;
-    
+
     // Mapping for token-specific URIs
     mapping(uint256 => string) private _tokenURIs;
 
@@ -16,9 +16,9 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
     mapping(uint256 => string) private _slotURIs;
 
     uint256 private _maxTotalValue;
-    
+
     uint256 private _minTransferValue;
-        
+
     uint256 private _currentTotalValue;
 
     constructor(
@@ -28,15 +28,21 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
         string memory baseURI_,
         uint256 maxTotalValue_,
         uint256 minTransferValue_
-    ) ERC3525SlotEnumerable(name_, symbol_, decimals_) Ownable(msg.sender){
-        require(minTransferValue_ > 0, "AVA_v1: min transfer value must be positive");
-        require(maxTotalValue_ % minTransferValue_ == 0, "AVA_v1: max total value must be divisible by min transfer value");
-        
+    ) ERC3525SlotEnumerable(name_, symbol_, decimals_) Ownable(msg.sender) {
+        require(
+            minTransferValue_ > 0,
+            "AVA_v1: min transfer value must be positive"
+        );
+        require(
+            maxTotalValue_ % minTransferValue_ == 0,
+            "AVA_v1: max total value must be divisible by min transfer value"
+        );
+
         _avaURI = baseURI_;
         _maxTotalValue = maxTotalValue_;
         _minTransferValue = minTransferValue_;
         _currentTotalValue = 0;
-    
+
         // Initialize the token ID generator，默认初次发售不超过10000个token
         _tokenIdGenerator = 10000;
     }
@@ -47,15 +53,14 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
     function maxTotalValue() public view returns (uint256) {
         return _maxTotalValue;
     }
-    
+
     function minTransferValue() public view returns (uint256) {
         return _minTransferValue;
     }
-    
+
     function currentTotalValue() public view returns (uint256) {
         return _currentTotalValue;
     }
-    
 
     // -------------------------------
     // URI Management
@@ -64,7 +69,7 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
         return _avaURI;
     }
 
-    function setBaseURI(string memory baseURI_) public onlyOwner{
+    function setBaseURI(string memory baseURI_) public onlyOwner {
         _avaURI = baseURI_;
     }
 
@@ -78,19 +83,31 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
         uint256 slot_,
         uint256 value_
     ) public onlyOwner {
-        require(_currentTotalValue + value_ <= _maxTotalValue, "AVA_v1: exceeds max total value");
-        
-        require(value_ % _minTransferValue == 0, "AVA_v1: value must be a multiple of min transfer value");
-                
+        require(
+            _currentTotalValue + value_ <= _maxTotalValue,
+            "AVA_v1: exceeds max total value"
+        );
+
+        require(
+            value_ % _minTransferValue == 0,
+            "AVA_v1: value must be a multiple of min transfer value"
+        );
+
         _mint(to_, tokenId_, slot_, value_);
         _currentTotalValue += value_;
     }
 
     function mintValue(uint256 tokenId_, uint256 value_) public onlyOwner {
-        require(_currentTotalValue + value_ <= _maxTotalValue, "AVA_v1: exceeds max total value");
-        
-        require(value_ % _minTransferValue == 0, "AVA_v1: value must be a multiple of min transfer value");
-        
+        require(
+            _currentTotalValue + value_ <= _maxTotalValue,
+            "AVA_v1: exceeds max total value"
+        );
+
+        require(
+            value_ % _minTransferValue == 0,
+            "AVA_v1: value must be a multiple of min transfer value"
+        );
+
         _mintValue(tokenId_, value_);
         _currentTotalValue += value_;
     }
@@ -103,8 +120,11 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
 
     function burnValue(uint256 tokenId_, uint256 burnValue_) public onlyOwner {
         // 检查burnValue是否为最小转账单位的整数倍
-        require(burnValue_ % _minTransferValue == 0, "AVA_v1: burn value must be a multiple of min transfer value");
-        
+        require(
+            burnValue_ % _minTransferValue == 0,
+            "AVA_v1: burn value must be a multiple of min transfer value"
+        );
+
         _burnValue(tokenId_, burnValue_);
         _currentTotalValue -= burnValue_;
     }
@@ -117,11 +137,14 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
         uint256 toTokenId_,
         uint256 value_
     ) public payable override(ERC3525, IERC3525) {
-        require(value_ % _minTransferValue == 0, "AVA_v1: transfer value must be a multiple of min transfer value");
-        
+        require(
+            value_ % _minTransferValue == 0,
+            "AVA_v1: transfer value must be a multiple of min transfer value"
+        );
+
         super.transferFrom(fromTokenId_, toTokenId_, value_);
     }
-    
+
     /**
      * @dev Override the transferFrom method of ERC3525 to ensure the transferred value complies with the minimum unit
      */
@@ -130,8 +153,11 @@ contract AVA_v1 is Context, Ownable, ERC3525SlotEnumerable {
         address to_,
         uint256 value_
     ) public payable override(ERC3525, IERC3525) returns (uint256) {
-        require(value_ % _minTransferValue == 0, "AVA_v1: transfer value must be a multiple of min transfer value");
-        
+        require(
+            value_ % _minTransferValue == 0,
+            "AVA_v1: transfer value must be a multiple of min transfer value"
+        );
+
         return super.transferFrom(fromTokenId_, to_, value_);
     }
 }
