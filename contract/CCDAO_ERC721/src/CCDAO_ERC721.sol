a540fe395473872082e214f97a4e0f3e0d608156
chtian <chtian@anfengde.com>
2025-06-19 10:59:54 +0800

    update README and create 721 code

---
+++ contract/CCDAO_ERC721/src/CCDAO_ERC721.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CCDAO_ERC721 is ERC721, Ownable {
+    uint256 private _nextTokenId;
+    string private _baseTokenURI;
+
+    constructor() ERC721("Cross Chain DAO NFT", "CCDAO") Ownable(msg.sender) {}
+
+    function mint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+    
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+    
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+}
